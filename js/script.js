72d9906c29ef00997afb90dc363104f01de7beb1
Mihail <mihail.skobkarev@krit.pro>
2021-04-02 11:08:26 +0400

    init commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// alert('Hello')
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Фамилия?', '');
+// answers[1] = prompt('Имя?', '');
+// answers[2] = prompt('Возраст?', '');
+
+// document.write(answers);
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+let incr = 10, decr = 10;
+// --decr;
+// ++incr;
+console.log(++incr);
+console.log(--decr);
+console.log(5/2);
